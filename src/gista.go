b2fa6ab878a25e31c0cad4cc512cf29619fb3951
zakester <Hadjammar.Hamza.Zakaria@gmail.com>
2024-02-23 14:33:35 +0100

    more work is done!

--- src/gista.go
+++ src/gista.go
@@ -1,14 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
 func main() {
-  var authors = Authors();
+	var authors = Authors()
 
-  ChangesInit(&authors[1])
+	var totalInsertions uint64
+	var totalDeletions uint64
+	var totalCommits uint64
 
-  println("Author:", authors[1].Name)
-  println("Commits:", authors[1].Commits)
+	for i := range authors {
+		ChangesInit(&authors[i])
 
-  println("Insertions:", authors[1].Insertions)
-  println("Deletions:", authors[1].Deletions)
+		totalCommits += uint64(authors[i].Commits)
+		totalInsertions += authors[i].Insertions
+		totalDeletions += authors[i].Deletions
+	}
 
+	println("Authors:", len(authors), "| Total Commits:", totalCommits, "| Total Insertions:", totalInsertions, "| Total Deletions:", totalDeletions, "| (Insertions + Deletions):", totalDeletions+totalInsertions)
+  println("--------------------------")
+
+  w := tabwriter.NewWriter(os.Stdout, 10, 1, 1, ' ', tabwriter.Debug)
+  fmt.Fprintln(w, "| Author\t Insertions\t Deletions\t Commits\t (Insertions+Deletions) |")
+	for _, author := range authors {
+    fmt.Fprintln(w, "|", author.Name, "\t", author.Insertions, "\t", author.Deletions, "\t", author.Commits, "\t", author.Insertions + author.Deletions)
+	}
+
+  w.Flush()
 }
